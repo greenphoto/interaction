cbc1d0db3c49e871663c1ed7aa39a8b6ba60c635
Jing <greenphoto3@gmail.com>
2015-01-19 16:10:24 -0500

    can log event with res id now

--- app/src/main/java/edu/uml/swin/logger/InteractionService.java
+++ app/src/main/java/edu/uml/swin/logger/InteractionService.java
@@ -5,6 +5,7 @@ import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.util.Log;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class InteractionService extends AccessibilityService {
 
@@ -26,8 +27,22 @@ public class InteractionService extends AccessibilityService {
                 return "TYPE_WINDOW_STATE_CHANGED";
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
                 return "TYPE_VIEW_TEXT_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                return "TYPE_VIEW_HOVER_ENTER";
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                return "TYPE_VIEW_HOVER_EXIT";
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                return "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+                return "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                return "TYPE_WINDOW_CONTENT_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                return "TYPE_VIEW_SCROLLED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                return "TYPE_VIEW_TEXT_SELECTION_CHANGED";
         }
-        return "default";
+        return "Default";
     }
 
     private String getEventText(AccessibilityEvent event) {
@@ -39,11 +54,83 @@ public class InteractionService extends AccessibilityService {
     }
 
     @Override
+    protected boolean onGesture(int gestureId){
+//        switch (gestureId){
+//            case GESTURE_SWIPE_DOWN:
+//                Log.v(TAG, "GESTURE_SWIPE_DOWN");
+//            case GESTURE_SWIPE_UP:
+//                Log.v(TAG, "GESTURE_SWIPE_UP");
+//            case GESTURE_SWIPE_LEFT:
+//                Log.v(TAG,"GESTURE_SWIPE_LEFT");
+//            case GESTURE_SWIPE_RIGHT:
+//                Log.v(TAG,"GESTURE_SWIPE_RIGHT");
+//            default:
+//                Log.v(TAG,"Other gestures.");
+//        }
+        Log.v(TAG, String.format("Gesture Id: %s", gestureId));
+        return true;
+    }
+
+    @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+//        if(getEventType(event).equalsIgnoreCase("Default")) return;
+        if(getEventType(event).equalsIgnoreCase("TYPE_WINDOW_CONTENT_CHANGED")) return;
         Log.v(TAG, String.format(
-                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
+                "[type] %s [class] %s [package] %s [time] %s [text] %s [windowId] %s",
                 getEventType(event), event.getClassName(), event.getPackageName(),
-                event.getEventTime(), getEventText(event)));
+                event.getEventTime(), getEventText(event), event.getWindowId()));
+
+        AccessibilityNodeInfo source = event.getSource();
+        if(source == null){
+            Log.v(TAG, "Failed to get source.");
+            return;
+        }
+        else{
+            Log.v(TAG, String.format("[SourceClass] %s [ViewId] %s",
+                    source.getClassName().toString(), source.getViewIdResourceName()));
+            Log.v(TAG,"--------------parents------------------");
+            AccessibilityNodeInfo parent = source.getParent();
+            while(parent!=null) {
+                Log.v(TAG, parent.getClassName().toString());
+                parent = parent.getParent();
+            }
+            Log.v(TAG,"--------------parents------------------");
+        }
+
+        if(getEventType(event).equalsIgnoreCase("TYPE_VIEW_CLICKED")) {
+            AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+            if(rootNode == null){
+                Log.v(TAG,"Root node info is null.");
+                return;
+            }
+            else{
+                recycle(rootNode, 1);
+            }
+            Log.v(TAG,"============================================");
+        }
+        source.recycle();
+    }
+
+    public void recycle(AccessibilityNodeInfo info, int level) {
+        String levelInfo = generateLevel(level);
+        Log.i(TAG, levelInfo+ "[ClassName] "+ info.getClassName()+" [Text] "+ info.getText() + " [WINDOW_ID] "+ info.getWindowId());
+
+        if(info.getChildCount()!=0){
+            for (int i = 0; i < info.getChildCount(); i++) {
+                if(info.getChild(i)!=null){
+                    recycle(info.getChild(i), level + 1);
+                }
+            }
+        }
+    }
+
+    private String generateLevel(int level){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < level; ++i){
+            sb.append(">--");
+        }
+
+        return sb.toString();
     }
 
     @Override
@@ -55,10 +142,10 @@ public class InteractionService extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
         Log.v(TAG, "onServiceConnected");
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.flags = AccessibilityServiceInfo.DEFAULT;
-        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
-        setServiceInfo(info);
+//        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+//        info.flags = AccessibilityServiceInfo.DEFAULT;
+//        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+//        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+//        setServiceInfo(info);
     }
 }
